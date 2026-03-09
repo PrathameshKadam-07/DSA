47a5e2c60fcc9af0c4013d65bf288a890c6c2786
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-03-09 19:44:53 +0530

    intersection of array

--- LEEDCODE/src/ArrayEasy/SingleNumber.java
+++ LEEDCODE/src/ArrayEasy/SingleNumber.java
@@ -1,5 +1,21 @@
 package ArrayEasy;
 
 public class SingleNumber {
-
+    public int singleNumber(int[] nums) {
+        for(int i = 0;i<nums.length-1;i++) {
+        	int cnt=0;
+        	for(int j = 0;j<nums.length-1;j++) {
+        		if(i==j) {
+        			continue;
+        		}
+        		else if(nums[i]==nums[j]) {
+        			cnt++;
+        		}
+        	}
+        	if(cnt==0) {
+        		return nums[i];
+        	}
+        }
+        
+    }
 }
