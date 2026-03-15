8b31db305c4a1bb549e3deb0d1a9f0d237eb3a86
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-03-16 02:05:47 +0530

    String reverse Word

---
+++ StriverSheet/src/String/ReverseWordsinaString.java
@@ -0,0 +1,21 @@
+package String;
+
+public class ReverseWordsinaString {
+	public String reverseWords(String s) {
+	s = s.trim();
+	String[] s1 = s.split("\\s+");
+	StringBuilder sb = new StringBuilder();
+	for (int i = s1.length - 1; i >= 0; i--) {
+		sb.append(s1[i]);
+		if (i != 0) {
+			sb.append(" ");
+		}
+	}
+
+	return sb.toString();
+}
+
+	public static void main(String[] args) {
+
+	}
+}
