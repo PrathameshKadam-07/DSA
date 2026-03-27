189a50abbcb8838604b84bc817486c16296b9969
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-03-28 01:29:55 +0530

    Check if Array Is Sorted and Rotated

---
+++ StriverSheet/src/Arrays/CheckifArrayIsSortedandRotated.java
@@ -0,0 +1,45 @@
+package Arrays;
+
+public class CheckifArrayIsSortedandRotated {
+	public boolean check(int[] nums) {
+		if(nums.length == 1){
+            return true;
+        }
+		for (int i = 0; i <= nums.length-1; i++) {
+            int cnt = 0;
+			for (int j = 0; j < nums.length - 1; j++) {
+				if (nums[j] <= nums[j + 1]) {
+					continue;
+				} else {
+					cnt++;
+					break;
+				}
+			}
+
+			if (cnt != 0) {
+				reverse(0, nums.length - 1, nums);
+				reverse(1, nums.length - 1, nums);
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public void reverse(int st, int end, int[] nums) {
+		while (st < end) {
+			int temp = nums[st];
+			nums[st] = nums[end];
+			nums[end] = temp;
+			st++;
+			end--;
+		}
+	}
+
+	public static void main(String[] args) {
+		CheckifArrayIsSortedandRotated ca = new CheckifArrayIsSortedandRotated();
+		int[] nums = { 2,1,3,4 };
+		System.out.println(ca.check(nums));
+	}
+}
