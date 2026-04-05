e61022a469470c1f846e92bc06f595be96a20a48
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-04-05 10:43:49 +0530

    169. Majority Element

---
+++ StriverSheet/src/Arrays/MajorityElement.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Map;
+
+public class MajorityElement {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        for(int x : nums){
+            if(hm.containsKey(x)){
+                hm.put(x,hm.get(x)+1);
+            }
+            else{
+                  hm.put(x,1);
+            }
+        }
+
+        int max = 0;
+
+        for(Map.Entry<Integer,Integer> m : hm.entrySet()){
+            int num = m.getValue();
+            if(max<num){
+                max = num;
+            }  
+        }
+            for(Map.Entry<Integer,Integer> m : hm.entrySet()){
+            if(max==m.getValue()){
+                return m.getKey();
+            }  
+        }
+
+    return -1;  
+    }
+}
