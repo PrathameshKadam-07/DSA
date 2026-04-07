e08048cd7bf89c3c3f516ce08dc469cedaa7650b
Prathamesh Kadam <146866950+PrathameshKadam-07@users.noreply.github.com>
2026-04-07 16:31:55 +0530

    Subarray Sum Equals K

--- StriverSheet/src/Arrays/spiralMatrix.java
+++ StriverSheet/src/Arrays/spiralMatrix.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 public class spiralMatrix {
 	 public List<Integer> spiralOrder(int[][] matrix) 
 	    {
-	        List<Integer> result = new ArrayList()<Integer>();
+		 
+		 List<Integer> result = new ArrayList<Integer>();
 
 	        int top = 0;
 	        int bottom = matrix.length - 1;
